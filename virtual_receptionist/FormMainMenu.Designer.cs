b3c360a3bde2fd908d69731526dd97679dbaaace
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-22 21:07:58 +0100

    + Menüsor és státuszsor hozzáadása a főmenü ablakhoz

--- virtual_receptionist/FormMainMenu.Designer.cs
+++ virtual_receptionist/FormMainMenu.Designer.cs
@@ -28,21 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStripMainMenu = new System.Windows.Forms.MenuStrip();
+            this.statusStripMainMenu = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
+            // 
+            // menuStripMainMenu
+            // 
+            this.menuStripMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMainMenu.Name = "menuStripMainMenu";
+            this.menuStripMainMenu.Size = new System.Drawing.Size(637, 24);
+            this.menuStripMainMenu.TabIndex = 0;
+            // 
+            // statusStripMainMenu
+            // 
+            this.statusStripMainMenu.Location = new System.Drawing.Point(0, 463);
+            this.statusStripMainMenu.Name = "statusStripMainMenu";
+            this.statusStripMainMenu.Size = new System.Drawing.Size(637, 22);
+            this.statusStripMainMenu.TabIndex = 1;
             // 
             // FormMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(637, 485);
+            this.Controls.Add(this.statusStripMainMenu);
+            this.Controls.Add(this.menuStripMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStripMainMenu;
             this.Name = "FormMainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Főmenü | Virtual Receptionist";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMainMenu;
+        private System.Windows.Forms.StatusStrip statusStripMainMenu;
     }
 }
