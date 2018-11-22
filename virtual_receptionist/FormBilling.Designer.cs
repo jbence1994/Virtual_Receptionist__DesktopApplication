7b10764d1993922739d217dd9ab0275811c6f47a
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-22 17:35:21 +0100

    + Vissza a főmenübe gomb hozzáadása a számlázó és szobakezelő formhoz

--- virtual_receptionist/FormBilling.Designer.cs
+++ virtual_receptionist/FormBilling.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonBackToMainMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonBackToMainMenu
+            // 
+            this.buttonBackToMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBackToMainMenu.Location = new System.Drawing.Point(310, 373);
+            this.buttonBackToMainMenu.Name = "buttonBackToMainMenu";
+            this.buttonBackToMainMenu.Size = new System.Drawing.Size(157, 64);
+            this.buttonBackToMainMenu.TabIndex = 6;
+            this.buttonBackToMainMenu.Text = "Vissza a főmenübe";
+            this.buttonBackToMainMenu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonBackToMainMenu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonBackToMainMenu.UseVisualStyleBackColor = true;
             // 
             // FormBilling
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 449);
+            this.Controls.Add(this.buttonBackToMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormBilling";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,5 +58,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonBackToMainMenu;
     }
 }
--- virtual_receptionist/FormRoomEditor.Designer.cs
+++ virtual_receptionist/FormRoomEditor.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonBackToMainMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonBackToMainMenu
+            // 
+            this.buttonBackToMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBackToMainMenu.Location = new System.Drawing.Point(335, 410);
+            this.buttonBackToMainMenu.Name = "buttonBackToMainMenu";
+            this.buttonBackToMainMenu.Size = new System.Drawing.Size(157, 64);
+            this.buttonBackToMainMenu.TabIndex = 6;
+            this.buttonBackToMainMenu.Text = "Vissza a főmenübe";
+            this.buttonBackToMainMenu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonBackToMainMenu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonBackToMainMenu.UseVisualStyleBackColor = true;
             // 
             // FormRoomEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(504, 486);
+            this.Controls.Add(this.buttonBackToMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormRoomEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Szoba kezelés | Virtual Receptionist";
+            this.Text = "Szobakezelés | Virtual Receptionist";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonBackToMainMenu;
     }
 }
