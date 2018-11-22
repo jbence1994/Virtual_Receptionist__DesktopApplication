35264c6dddaca92d6f7b4b984deeef75c3fb4fdf
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-22 14:59:53 +0100

    + ListView GUI veézrlő hozzáadása a vendégadatbázis ablakhoz (engedélyezett FullRowSelect, GridLines és 'Details' tulajdonság)

--- virtual_receptionist/FormGuestDatabase.Designer.cs
+++ virtual_receptionist/FormGuestDatabase.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(261, 457);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // FormGuestDatabase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 482);
+            this.Controls.Add(this.listView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormGuestDatabase";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
