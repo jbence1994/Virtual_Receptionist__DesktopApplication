71d08514c27aca7080bfcd71e21ad18cd1437e28
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-25 16:07:10 +0100

    + Gombok beállítása

--- virtual_receptionist/FormUnregisteredSoftware.Designer.cs
+++ virtual_receptionist/FormUnregisteredSoftware.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonRegister.Location = new System.Drawing.Point(12, 204);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(124, 23);
+            this.buttonRegister.TabIndex = 0;
+            this.buttonRegister.Text = "Szoftver regisztrálása";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(257, 204);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(124, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Mégse";
+            this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // FormUnregisteredSoftware
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(393, 239);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonRegister);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormUnregisteredSoftware";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,5 +68,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonCancel;
     }
 }
