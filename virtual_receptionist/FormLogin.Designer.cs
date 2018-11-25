b9187a2b5981711bd2ff3255172bd33906059b31
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-25 20:50:07 +0100

    + Login form redesign

--- virtual_receptionist/FormLogin.Designer.cs
+++ virtual_receptionist/FormLogin.Designer.cs
@@ -31,37 +31,49 @@
             this.buttonLogin = new System.Windows.Forms.Button();
             this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(95, 117);
+            this.buttonLogin.Location = new System.Drawing.Point(48, 140);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(182, 23);
+            this.buttonLogin.Size = new System.Drawing.Size(278, 23);
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Belépés";
             this.buttonLogin.UseVisualStyleBackColor = true;
             // 
             // textBoxUsername
             // 
-            this.textBoxUsername.Location = new System.Drawing.Point(95, 65);
+            this.textBoxUsername.Location = new System.Drawing.Point(48, 88);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(182, 20);
+            this.textBoxUsername.Size = new System.Drawing.Size(278, 20);
             this.textBoxUsername.TabIndex = 1;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(95, 91);
+            this.textBoxPassword.Location = new System.Drawing.Point(48, 114);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(182, 20);
+            this.textBoxPassword.Size = new System.Drawing.Size(278, 20);
             this.textBoxPassword.TabIndex = 2;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTitle.Location = new System.Drawing.Point(44, 45);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(282, 24);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Üdvözli a Virtual Receptionst!";
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 218);
+            this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.buttonLogin);
@@ -79,6 +91,7 @@
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.TextBox textBoxUsername;
         private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelTitle;
     }
 }
 
