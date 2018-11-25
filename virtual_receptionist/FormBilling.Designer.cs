deedbbb2a0dc7bf53909e29e1bf8fbbc799fd509
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-25 21:35:57 +0100

    + Design
    + Ikonok
    
    + Számlázó form design

--- virtual_receptionist/FormBilling.Designer.cs
+++ virtual_receptionist/FormBilling.Designer.cs
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBilling));
             this.buttonBackToMainMenu = new System.Windows.Forms.Button();
+            this.dataGridViewItems = new System.Windows.Forms.DataGridView();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonAddItem = new System.Windows.Forms.Button();
+            this.buttonUpdateItem = new System.Windows.Forms.Button();
+            this.buttonDeleteItem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewItems)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonBackToMainMenu
             // 
             this.buttonBackToMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonBackToMainMenu.Location = new System.Drawing.Point(310, 373);
+            this.buttonBackToMainMenu.Location = new System.Drawing.Point(664, 420);
             this.buttonBackToMainMenu.Name = "buttonBackToMainMenu";
             this.buttonBackToMainMenu.Size = new System.Drawing.Size(157, 64);
             this.buttonBackToMainMenu.TabIndex = 6;
@@ -43,16 +50,82 @@
             this.buttonBackToMainMenu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.buttonBackToMainMenu.UseVisualStyleBackColor = true;
             // 
+            // dataGridViewItems
+            // 
+            this.dataGridViewItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewItems.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewItems.Name = "dataGridViewItems";
+            this.dataGridViewItems.Size = new System.Drawing.Size(809, 402);
+            this.dataGridViewItems.TabIndex = 7;
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonPrint.Image = ((System.Drawing.Image)(resources.GetObject("buttonPrint.Image")));
+            this.buttonPrint.Location = new System.Drawing.Point(501, 420);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(157, 64);
+            this.buttonPrint.TabIndex = 8;
+            this.buttonPrint.Text = "Számla nyomtatása";
+            this.buttonPrint.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddItem
+            // 
+            this.buttonAddItem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonAddItem.Image = ((System.Drawing.Image)(resources.GetObject("buttonAddItem.Image")));
+            this.buttonAddItem.Location = new System.Drawing.Point(12, 420);
+            this.buttonAddItem.Name = "buttonAddItem";
+            this.buttonAddItem.Size = new System.Drawing.Size(157, 64);
+            this.buttonAddItem.TabIndex = 9;
+            this.buttonAddItem.Text = "Tétel hozzáadása";
+            this.buttonAddItem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonAddItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonAddItem.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateItem
+            // 
+            this.buttonUpdateItem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUpdateItem.Image = ((System.Drawing.Image)(resources.GetObject("buttonUpdateItem.Image")));
+            this.buttonUpdateItem.Location = new System.Drawing.Point(175, 420);
+            this.buttonUpdateItem.Name = "buttonUpdateItem";
+            this.buttonUpdateItem.Size = new System.Drawing.Size(157, 64);
+            this.buttonUpdateItem.TabIndex = 10;
+            this.buttonUpdateItem.Text = "Tétel módosítása";
+            this.buttonUpdateItem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonUpdateItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonUpdateItem.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteItem
+            // 
+            this.buttonDeleteItem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("buttonDeleteItem.Image")));
+            this.buttonDeleteItem.Location = new System.Drawing.Point(338, 420);
+            this.buttonDeleteItem.Name = "buttonDeleteItem";
+            this.buttonDeleteItem.Size = new System.Drawing.Size(157, 64);
+            this.buttonDeleteItem.TabIndex = 11;
+            this.buttonDeleteItem.Text = "Tétel törlése";
+            this.buttonDeleteItem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonDeleteItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonDeleteItem.UseVisualStyleBackColor = true;
+            // 
             // FormBilling
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 449);
+            this.ClientSize = new System.Drawing.Size(833, 496);
+            this.Controls.Add(this.buttonDeleteItem);
+            this.Controls.Add(this.buttonUpdateItem);
+            this.Controls.Add(this.buttonAddItem);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.dataGridViewItems);
             this.Controls.Add(this.buttonBackToMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormBilling";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Számlázó | Virtual Receptionist";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewItems)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +133,10 @@
         #endregion
 
         private System.Windows.Forms.Button buttonBackToMainMenu;
+        private System.Windows.Forms.DataGridView dataGridViewItems;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonAddItem;
+        private System.Windows.Forms.Button buttonUpdateItem;
+        private System.Windows.Forms.Button buttonDeleteItem;
     }
 }
--- virtual_receptionist/Program.cs
+++ virtual_receptionist/Program.cs
@@ -16,7 +16,7 @@ namespace virtual_receptionist
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormGuestDatabase());
+            Application.Run(new FormBilling());
         }
     }
 }
