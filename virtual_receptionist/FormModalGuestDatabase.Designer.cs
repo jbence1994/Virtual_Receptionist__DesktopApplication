329c521e3282e03cc09a593886c1df13069fd29d
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-26 20:58:06 +0100

    + Vendégadatbázis modális ablak redesign

--- virtual_receptionist/FormModalGuestDatabase.Designer.cs
+++ virtual_receptionist/FormModalGuestDatabase.Designer.cs
@@ -226,7 +226,7 @@
             // buttonOK
             // 
             this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOK.Location = new System.Drawing.Point(132, 299);
+            this.buttonOK.Location = new System.Drawing.Point(12, 309);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 29;
@@ -236,7 +236,7 @@
             // buttonCancel
             // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(242, 299);
+            this.buttonCancel.Location = new System.Drawing.Point(310, 309);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 30;
@@ -247,7 +247,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 357);
+            this.ClientSize = new System.Drawing.Size(397, 344);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.checkBoxCorporateGuest);
