0894cd6121940bfa9315bdb1208e423b84e9e4c7
Bence Juhász <juhasz.bence@outlook.hu>
2018-11-26 21:00:01 +0100

    + BillingController osztály fejl. + makrózás

--- virtual_receptionist/BillingController.cs
+++ virtual_receptionist/BillingController.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace virtual_receptionist
 {
+    /// <summary>
+    /// Számlázási folyamatokat vezérlő osztály
+    /// </summary>
     public class BillingController
     {
+        #region Adattagok
 
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// BillingController osztály üres konstruktora
+        /// </summary>
+        public BillingController()
+        {
+
+        }
+
+        #endregion
+
+        #region Metódusok
+
+
+        #endregion
     }
 }
