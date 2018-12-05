90d168dc4d8b29bd4eef3d8265fb2861740c32b8
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-05 03:42:17 +0100

    + Számlázó modul adattárának kifejl.

--- virtual_receptionist/DataStoreMain.cs
+++ virtual_receptionist/DataStoreMain.cs
@@ -97,46 +97,6 @@ namespace virtual_receptionist
 
             return countries;
         }
-        /// <summary>
-        /// Metódus, amely adatbázisból kiolvassa a számlázási tételeket és egy DataTable adatszerkezetbe menti őket
-        /// </summary>
-        /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
-        public DataTable GetBillingItems() //<= UNIT TEST !!!
-        {
-            DataTable billingItems = new DataTable();
-
-            string item = string.Empty;
-            string price = string.Empty;
-            string unit = string.Empty;
-
-            mySqlConnection.Open();
-            Debug.WriteLine("Sikeres adatbázis kapcsolódás...");
-
-            mySqlCommand = new MySqlCommand()
-            {
-                CommandText = "SELECT * FROM billing_items",
-                Connection = mySqlConnection
-            };
-
-            mySqlDataReader = mySqlCommand.ExecuteReader();
-            Debug.WriteLine("MySqlDataReader olvasás sikeresen elindult...");
-
-            while (mySqlDataReader.Read())
-            {
-                item = mySqlDataReader["item"].ToString();
-                price = mySqlDataReader["price"].ToString();
-                unit = mySqlDataReader["unit"].ToString();
-
-            }
-
-            mySqlDataReader.Close();
-            Debug.WriteLine("MySqlDataReader olvasás sikeresen befejeződött...");
-
-            mySqlConnection.Close();
-            Debug.WriteLine("Adatbázis kapcsolat sikeresen lezárult...");
-
-            return billingItems;
-        }
 
         #endregion
     }
