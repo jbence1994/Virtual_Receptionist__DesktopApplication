e48ab8d1fbc9fd59a251cc165c564e1fe6e44141
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-06 00:14:24 +0100

    + Reservation model class implementálása (final)

--- virtual_receptionist/Reservation.cs
+++ virtual_receptionist/Reservation.cs
@@ -43,7 +43,11 @@ namespace virtual_receptionist
         /// </summary>
         public Reservation(Guest guest, Room room, int numberOfGuest, DateTime arrival, DateTime departure)
         {
-
+            this.guest = guest;
+            this.room = room;
+            this.numberOfGuest = numberOfGuest;
+            this.arrival = arrival;
+            this.departure = departure;
         }
         /// <summary>
         /// Reservation osztály üres konstruktora
@@ -57,17 +61,89 @@ namespace virtual_receptionist
 
         #region Getter és setter tulajdonságok
 
-
+        /// <summary>
+        /// Guest modell osztály egy példánya
+        /// </summary>
+        public Guest Guest
+        {
+            get
+            {
+                return guest;
+            }
+            set
+            {
+                guest = value;
+            }
+        }
+        /// <summary>
+        /// Room modell osztály egy példánya
+        /// </summary>
+        public Room Room
+        {
+            get
+            {
+                return room;
+            }
+            set
+            {
+                room = value;
+            }
+        }
+        /// <summary>
+        /// Vendégek száma
+        /// </summary>
+        public int NumberOfGuest
+        {
+            get
+            {
+                return numberOfGuest;
+            }
+            set
+            {
+                numberOfGuest = value;
+            }
+        }
+        /// <summary>
+        /// Érkezés dátuma
+        /// </summary>
+        public DateTime Arrival
+        {
+            get
+            {
+                return arrival;
+            }
+            set
+            {
+                arrival = value;
+            }
+        }
+        /// <summary>
+        /// Távozás dátuma
+        /// </summary>
+        public DateTime Departure
+        {
+            get
+            {
+                return departure;
+            }
+            set
+            {
+                departure = value;
+            }
+        }
 
         #endregion
 
-
         #region Metódusok
 
-
-
-
-
+        /// <summary>
+        /// Reservation osztályból készült objektum string típusúvá alakítása
+        /// </summary>
+        /// <returns>Visszaadja a Reservation típusú objektumot string típusúra alakítva</returns>
+        public override string ToString()
+        {
+            return guest + "" + room + "" + numberOfGuest + "" + arrival + "" + departure;
+        }
 
         #endregion
     }
