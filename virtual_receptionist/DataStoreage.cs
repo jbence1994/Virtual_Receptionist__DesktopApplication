b0a6ac65b38f8db835fb6b2193385cfccf145cb7
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-05 03:11:51 +0100

    + Szótár adatszerkezet megváltoztatása egyszerű generikus listává

--- virtual_receptionist/DataStoreage.cs
+++ virtual_receptionist/DataStoreage.cs
@@ -58,10 +58,10 @@ namespace virtual_receptionist
         /// Metódus, amely adatbázisból kiolvassa az országok kódját és nevét és egy Dictionary adatszerkezetbe menti őket
         /// </summary>
         /// <returns>Adatokkal feltöltött Dictionary-t adja vissza</returns>
-        public Dictionary<string, string> GetCountries() // <= UNIT TEST !!!
+        public List<string> GetCountries() // <= UNIT TEST !!!
         {
-            Dictionary<string, string> countries = new Dictionary<string, string>();
-            string countryCode = string.Empty;
+            List<string> countries = new List<string>();
+
             string countryName = string.Empty;
 
             mySqlConnection.Open();
@@ -78,11 +78,9 @@ namespace virtual_receptionist
 
             while (mySqlDataReader.Read())
             {
-                countryCode = mySqlDataReader["Code"].ToString();
                 countryName = mySqlDataReader["Name"].ToString();
+                countries.Add(countryName);
             }
-
-            countries.Add(countryCode, countryName);
 
             mySqlDataReader.Close();
             Debug.WriteLine("MySqlDataReader olvasás sikeresen befejeződött...");
