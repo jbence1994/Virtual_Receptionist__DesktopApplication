fbb0e05d29d4e3f916126c7a1c08b418ba74807c
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-05 22:15:01 +0100

    + FormModalBillingItems formon ListView GUI vezérlő átnevezése
    + Feltöltő algoritmus kommentben elhelyezés

--- virtual_receptionist/FormModalBillingItems.Designer.cs
+++ virtual_receptionist/FormModalBillingItems.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listViewItems = new System.Windows.Forms.ListView();
+            this.listViewBillingItems = new System.Windows.Forms.ListView();
             this.columnHeaderItem = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderUnit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
-            // listViewItems
+            // listViewBillingItems
             // 
-            this.listViewItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listViewBillingItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeaderItem,
             this.columnHeaderPrice,
             this.columnHeaderUnit});
-            this.listViewItems.FullRowSelect = true;
-            this.listViewItems.GridLines = true;
-            this.listViewItems.Location = new System.Drawing.Point(13, 13);
-            this.listViewItems.MultiSelect = false;
-            this.listViewItems.Name = "listViewItems";
-            this.listViewItems.Size = new System.Drawing.Size(468, 354);
-            this.listViewItems.TabIndex = 0;
-            this.listViewItems.UseCompatibleStateImageBehavior = false;
-            this.listViewItems.View = System.Windows.Forms.View.Details;
+            this.listViewBillingItems.FullRowSelect = true;
+            this.listViewBillingItems.GridLines = true;
+            this.listViewBillingItems.Location = new System.Drawing.Point(13, 13);
+            this.listViewBillingItems.MultiSelect = false;
+            this.listViewBillingItems.Name = "listViewBillingItems";
+            this.listViewBillingItems.Size = new System.Drawing.Size(468, 354);
+            this.listViewBillingItems.TabIndex = 0;
+            this.listViewBillingItems.UseCompatibleStateImageBehavior = false;
+            this.listViewBillingItems.View = System.Windows.Forms.View.Details;
             // 
             // columnHeaderItem
             // 
@@ -70,7 +70,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(493, 379);
-            this.Controls.Add(this.listViewItems);
+            this.Controls.Add(this.listViewBillingItems);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormModalBillingItems";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -82,7 +82,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listViewItems;
+        private System.Windows.Forms.ListView listViewBillingItems;
         private System.Windows.Forms.ColumnHeader columnHeaderItem;
         private System.Windows.Forms.ColumnHeader columnHeaderPrice;
         private System.Windows.Forms.ColumnHeader columnHeaderUnit;
