86d0558c26dd6eb4f4a90dd834895008310aae3d
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-06 01:55:29 +0100

    + Vendégadatbázis lementése Lista adatszerkezetbe

--- virtual_receptionist/DataStoreGuest.cs
+++ virtual_receptionist/DataStoreGuest.cs
@@ -1,14 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace virtual_receptionist
 {
     public partial class DataStore
     {
         #region Vendégadatbázis-szerkesztő modul metódusai
+
+        public List<Guest> GetGuests()
+        {
+            List<Guest> guests = new List<Guest>();
+
+            mySqlConnection.Open();
+
+            mySqlCommand = new MySqlCommand()
+            {
+                CommandText = "SELECT * FROM guest",
+                Connection = mySqlConnection
+            };
+
+            mySqlDataReader = mySqlCommand.ExecuteReader();
+
+            while (mySqlDataReader.Read())
+            {
+                Guest guest = new Guest();
+
+                guest.Name = mySqlDataReader["Name"].ToString();
+
+                if (mySqlDataReader["Nationality"].ToString() == "0")
+                {
+                    guest.Nationality = true;
+                }
+                else
+                {
+                    guest.Nationality = false;
+                }
+
+                guest.Address = mySqlDataReader["Address"].ToString();
+                guest.City = mySqlDataReader["City"].ToString();
+                guest.ZipCode = mySqlDataReader["ZipCode"].ToString();
+                guest.Country = mySqlDataReader["Country"].ToString();
+                guest.VatNumber = mySqlDataReader["VATNumber"].ToString();
+                guest.PhoneNumber = mySqlDataReader["PhoneNumber"].ToString();
+                guest.EmailAddress = mySqlDataReader["EmailAddress"].ToString();
+
+                guests.Add(guest);
+            }
+
+            mySqlConnection.Close();
+
+            return guests;
+        }
 
         #endregion
     }
