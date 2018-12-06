87f87beb8d70a6df45ce90106df9777d91335314
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-06 02:46:36 +0100

    Adatbázis konfiguráció form
    + Kapcsolat leíró XML fájl csatolása
    + GUI design

--- virtual_receptionist/FormDatabaseConfiguration.Designer.cs
+++ virtual_receptionist/FormDatabaseConfiguration.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDatabaseConfiguration));
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxDatabase = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(12, 39);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(217, 20);
+            this.textBoxServer.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(12, 143);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(217, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 117);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(217, 20);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(12, 91);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(217, 20);
+            this.textBoxUsername.TabIndex = 6;
+            // 
+            // textBoxDatabase
+            // 
+            this.textBoxDatabase.Location = new System.Drawing.Point(12, 65);
+            this.textBoxDatabase.Name = "textBoxDatabase";
+            this.textBoxDatabase.Size = new System.Drawing.Size(217, 20);
+            this.textBoxDatabase.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Helyi",
+            "Távoli"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonConnect.Image = ((System.Drawing.Image)(resources.GetObject("buttonConnect.Image")));
+            this.buttonConnect.Location = new System.Drawing.Point(235, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(217, 151);
+            this.buttonConnect.TabIndex = 9;
+            this.buttonConnect.Text = "Csatlakozás";
+            this.buttonConnect.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonConnect.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // FormDatabaseConfiguration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormDatabaseConfiguration";
+            this.ClientSize = new System.Drawing.Size(461, 169);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxDatabase);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxServer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormDatabaseConfiguration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adatbázis konfiguráció | Virtual Receptionist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxDatabase;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonConnect;
     }
 }
