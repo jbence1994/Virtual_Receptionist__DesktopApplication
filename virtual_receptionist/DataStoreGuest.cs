311ad534aedb3450edff80b698cbc755e44ca295
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-06 01:40:08 +0100

    + Vendégadatbázis-szerkesztő modulra szabott adattár töredék osztály létrehozása

---
+++ virtual_receptionist/DataStoreGuest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_receptionist
+{
+    public partial class DataStoreGuest
+    {
+        #region Vendégadatbázis-szerkesztő modul metódusai
+
+        #endregion
+    }
+}
