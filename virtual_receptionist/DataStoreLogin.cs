ccb894b78a5368119945abf849993cd8649b9714
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-08 11:10:13 +0100

    + Fejl.
    + Szükséges névterek törlése
    + Metódus impl.

--- virtual_receptionist/DataStoreLogin.cs
+++ virtual_receptionist/DataStoreLogin.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
 
 namespace virtual_receptionist
 {
-    class DataStoreLogin
+    public partial class DataStore
     {
+        #region Belépési folyamatot vezérlő és ellenőrző metódusok
+
+        public bool IsAnyAccountExist()
+        {
+
+
+
+            return false;
+        }
+
+        #endregion
     }
 }
