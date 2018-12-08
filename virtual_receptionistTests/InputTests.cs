244e8d57b84bf9b7628af7c557b03624904db975
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-12-08 13:14:43 +0100

    + Sikeres unit tesztmetódus
    [Üres bemenet-re dob kivételt és le lehet kezelni]

--- virtual_receptionistTests/InputTests.cs
+++ virtual_receptionistTests/InputTests.cs
@@ -5,21 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace virtual_receptionist.Tests
 {
     [TestClass()]
     public class InputTests
     {
+        /// <summary>
+        /// Tesztmetódus: Üres inputra kiváltódik-e kivétel a függvény futásakor?
+        /// </summary>
         [TestMethod()]
         public void InputControllerTest_InCaseInputIsEmpty()
         {
-            Input input = new Input("");
-            bool expected = true;
-            bool actual = input.InputController();
-            Assert.AreEqual(expected, actual, "Nem jelez hibá üres hiba esetén!");
-
-
+            try
+            {
+                Input input = new Input("");
+                bool expected = true;
+                bool actual = input.InputController();
+                Assert.AreEqual(actual, expected, "Nem jelez hibá üres hiba esetén!");
+            }
+            catch (Exception)
+            {
+                Debug.Write("Kivétel elkapva!");
+            }
         }
     }
 }
