bcb7b6ca0b938bc4c0160f6f63d5d5a79a371652
jbence1994 <juhasz.bence.zsolt@gmail.com>
2018-12-25 02:16:16 +0100

    Szobakiadásokat adatbázisból kiolvasó metódus impl.

--- virtual_receptionist/DataStoreReservation.cs
+++ virtual_receptionist/DataStoreReservation.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics;
 
 namespace virtual_receptionist
 {
@@ -7,7 +8,41 @@ namespace virtual_receptionist
     {
         #region Foglalás-kezelő modul metódusai
 
+        /// <summary>
+        /// Metódus, amely visszaadja az adatbázisban tárolt összes szobakiadás adatát egy DataTable adatszerkezetben
+        /// </summary>
+        /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
+        public DataTable GetRents()
+        {
+            DataTable reservations = new DataTable();
 
+            mySqlConnection.Open();
+            Debug.WriteLine("Sikeres adatbázis kapcsolódás...");
+
+            mySqlCommand = new MySqlCommand()
+            {
+                CommandText = "SELECT * FROM reservation",
+                Connection = mySqlConnection
+            };
+
+            mySqlDataAdapter = new MySqlDataAdapter()
+            {
+                SelectCommand = mySqlCommand
+            };
+
+            mySqlCommandBuilder = new MySqlCommandBuilder()
+            {
+                DataAdapter = mySqlDataAdapter
+            };
+
+            mySqlDataAdapter.Fill(reservations);
+
+            mySqlConnection.Close();
+            Debug.WriteLine("Adatbázis kapcsolat sikeresen lezárult...");
+
+
+            return reservations;
+        }
 
         #endregion
     }
