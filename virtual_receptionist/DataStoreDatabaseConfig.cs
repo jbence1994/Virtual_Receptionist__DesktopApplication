b87565d1fd79f970e6714a0eb12bb991d158f28a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2018-12-25 15:12:30 +0100

    DataStoreDatabaseConfig metódusainak kommentezése

--- virtual_receptionist/DataStoreDatabaseConfig.cs
+++ virtual_receptionist/DataStoreDatabaseConfig.cs
@@ -12,30 +12,34 @@ namespace virtual_receptionist
     {
         #region Adatbázis konfigurációhoz szükséges metódusok
 
+        /// <summary>
+        /// Metódus, amely helyi adatbázis kiszolgáló útvonalát adja vissza XML állományból
+        /// </summary>
+        /// <returns>A kiszolgáló útvonalát adja vissza karakterláncként</returns>
         public string GetLocalDatabaseServerPathFromXML()
         {
             throw new NotImplementedException();
         }
         /// <summary>
-        /// 
+        /// Metódus, amely távoli adatbázis kiszolgáló útvonalát adja vissza XML állományból
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A kiszolgáló útvonalát adja vissza karakterláncként</returns>
         public string GetRemoteDatabaseServerPathFromXML()
         {
             throw new NotImplementedException();
         }
         /// <summary>
-        /// 
+        /// Metódus, amely helyi adatbázis kiszolgáló útvonalát adja vissza szöveges állományból
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A kiszolgáló útvonalát adja vissza karakterláncként</returns>
         public string GetLocalDatabaseServerPathFromTxt()
         {
             throw new NotImplementedException();
         }
         /// <summary>
-        /// 
+        /// Metódus, amely távoli adatbázis kiszolgáló útvonalát adja vissza szöveges állományból
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A kiszolgáló útvonalát adja vissza karakterláncként</returns>
         public string GetRemoteDatabaseServerPathFromTxt()
         {
             throw new NotImplementedException();
