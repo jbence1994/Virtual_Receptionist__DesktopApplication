2600fea62fc5c0a118d98ea803a4791efc0a1d1d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2019-01-06 00:20:04 +0100

    Vendégadatbázis UI redesign

--- virtual_receptionist/FormGuestDatabase.Designer.cs
+++ virtual_receptionist/FormGuestDatabase.Designer.cs
@@ -42,7 +42,6 @@
             this.buttonAddGuest = new System.Windows.Forms.Button();
             this.buttonDeleteGuest = new System.Windows.Forms.Button();
             this.buttonUpdateGuest = new System.Windows.Forms.Button();
-            this.menuStripGuestDatabase = new System.Windows.Forms.MenuStrip();
             this.buttonBackToMainMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -60,9 +59,9 @@
             this.columnHeaderEmailAddress});
             this.listViewGuests.FullRowSelect = true;
             this.listViewGuests.GridLines = true;
-            this.listViewGuests.Location = new System.Drawing.Point(12, 27);
+            this.listViewGuests.Location = new System.Drawing.Point(12, 12);
             this.listViewGuests.Name = "listViewGuests";
-            this.listViewGuests.Size = new System.Drawing.Size(985, 549);
+            this.listViewGuests.Size = new System.Drawing.Size(995, 560);
             this.listViewGuests.TabIndex = 0;
             this.listViewGuests.UseCompatibleStateImageBehavior = false;
             this.listViewGuests.View = System.Windows.Forms.View.Details;
@@ -117,7 +116,7 @@
             // 
             this.buttonAddGuest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonAddGuest.Image = ((System.Drawing.Image)(resources.GetObject("buttonAddGuest.Image")));
-            this.buttonAddGuest.Location = new System.Drawing.Point(1003, 27);
+            this.buttonAddGuest.Location = new System.Drawing.Point(1013, 12);
             this.buttonAddGuest.Name = "buttonAddGuest";
             this.buttonAddGuest.Size = new System.Drawing.Size(157, 64);
             this.buttonAddGuest.TabIndex = 1;
@@ -131,7 +130,7 @@
             // 
             this.buttonDeleteGuest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonDeleteGuest.Image = ((System.Drawing.Image)(resources.GetObject("buttonDeleteGuest.Image")));
-            this.buttonDeleteGuest.Location = new System.Drawing.Point(1003, 167);
+            this.buttonDeleteGuest.Location = new System.Drawing.Point(1013, 152);
             this.buttonDeleteGuest.Name = "buttonDeleteGuest";
             this.buttonDeleteGuest.Size = new System.Drawing.Size(157, 64);
             this.buttonDeleteGuest.TabIndex = 2;
@@ -145,7 +144,7 @@
             // 
             this.buttonUpdateGuest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonUpdateGuest.Image = ((System.Drawing.Image)(resources.GetObject("buttonUpdateGuest.Image")));
-            this.buttonUpdateGuest.Location = new System.Drawing.Point(1003, 97);
+            this.buttonUpdateGuest.Location = new System.Drawing.Point(1013, 82);
             this.buttonUpdateGuest.Name = "buttonUpdateGuest";
             this.buttonUpdateGuest.Size = new System.Drawing.Size(157, 64);
             this.buttonUpdateGuest.TabIndex = 3;
@@ -155,18 +154,11 @@
             this.buttonUpdateGuest.UseVisualStyleBackColor = true;
             this.buttonUpdateGuest.Click += new System.EventHandler(this.buttonUpdateGuest_Click);
             // 
-            // menuStripGuestDatabase
-            // 
-            this.menuStripGuestDatabase.Location = new System.Drawing.Point(0, 0);
-            this.menuStripGuestDatabase.Name = "menuStripGuestDatabase";
-            this.menuStripGuestDatabase.Size = new System.Drawing.Size(1167, 24);
-            this.menuStripGuestDatabase.TabIndex = 4;
-            // 
             // buttonBackToMainMenu
             // 
             this.buttonBackToMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonBackToMainMenu.Image = ((System.Drawing.Image)(resources.GetObject("buttonBackToMainMenu.Image")));
-            this.buttonBackToMainMenu.Location = new System.Drawing.Point(1003, 508);
+            this.buttonBackToMainMenu.Location = new System.Drawing.Point(1013, 508);
             this.buttonBackToMainMenu.Name = "buttonBackToMainMenu";
             this.buttonBackToMainMenu.Size = new System.Drawing.Size(157, 64);
             this.buttonBackToMainMenu.TabIndex = 5;
@@ -180,21 +172,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1167, 584);
+            this.ClientSize = new System.Drawing.Size(1180, 584);
             this.Controls.Add(this.buttonBackToMainMenu);
             this.Controls.Add(this.buttonUpdateGuest);
             this.Controls.Add(this.buttonDeleteGuest);
             this.Controls.Add(this.buttonAddGuest);
             this.Controls.Add(this.listViewGuests);
-            this.Controls.Add(this.menuStripGuestDatabase);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MainMenuStrip = this.menuStripGuestDatabase;
             this.Name = "FormGuestDatabase";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Vendégadatbázis | Virtual Receptionist";
             this.Load += new System.EventHandler(this.FormGuestDatabase_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -204,7 +193,6 @@
         private System.Windows.Forms.Button buttonAddGuest;
         private System.Windows.Forms.Button buttonDeleteGuest;
         private System.Windows.Forms.Button buttonUpdateGuest;
-        private System.Windows.Forms.MenuStrip menuStripGuestDatabase;
         private System.Windows.Forms.Button buttonBackToMainMenu;
         private System.Windows.Forms.ColumnHeader columnHeaderName;
         private System.Windows.Forms.ColumnHeader columnHeaderNationality;
