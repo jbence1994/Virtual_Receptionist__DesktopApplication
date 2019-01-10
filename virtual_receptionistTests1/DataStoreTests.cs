c63ed1c5c16577ac9750261a9f0f2f82b12a6345
jbence1994 <juhasz.bence.zsolt@gmail.com>
2019-01-10 15:53:57 +0100

    + Branch init

---
+++ virtual_receptionistTests1/DataStoreTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using virtual_receptionist.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_receptionist.Model.Tests
+{
+    [TestClass()]
+    public class DataStoreTests
+    {
+        [TestMethod()]
+        public void CountItemDiscountTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
