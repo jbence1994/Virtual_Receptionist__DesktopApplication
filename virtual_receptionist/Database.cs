4e8d7fe73ded0bc4ba2150417fe8a180197b588a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2019-01-14 01:30:09 +0100

    ORM class init

---
+++ virtual_receptionist/Database.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace virtual_receptionist.Model
+{
+    /// <summary>
+    /// Adatbázis műveleteket és interakciókat végző ORM osztály
+    /// </summary>
+    public static class Database
+    {
+        /// <summary>
+        /// Adatbázis kapcsolatot létrehozó mező
+        /// </summary>
+        private static MySqlConnection mySqlConnection;
+        /// <summary>
+        /// SQL utasítást vágrehajtó mező
+        /// </summary>
+        private static MySqlCommand mySqlCommand;
+        /// <summary>
+        /// Adatbázis adatot olvasó mező
+        /// </summary>
+        private static MySqlDataReader mySqlDataReader;
+        /// <summary>
+        /// Adatszerkezeteket adattal feltöltő osztály egy példánya
+        /// </summary>
+        private static MySqlDataAdapter mySqlDataAdapter;
+        /// <summary>
+        /// SQL DML és DDL parancsokat egyszerűen végrehajtó osztály egy példánya
+        /// </summary>
+        private static MySqlCommandBuilder mySqlCommandBuilder;
+
+        /// <summary>
+        /// Adatbázis műveleteket és interakciókat végző ORM osztály statikus konstruktora
+        /// </summary>
+        static Database()
+        {
+            mySqlConnection = new MySqlConnection()
+            {
+                ConnectionString = ""
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void SetConnectionString()
+        {
+
+        }
+    }
+}
