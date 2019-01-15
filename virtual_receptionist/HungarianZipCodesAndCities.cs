3a1df2a4fe21bb48c510dcf856c292362ec1b3a3
jbence1994 <juhasz.bence.zsolt@gmail.com>
2019-01-15 14:45:30 +0100

    Magyarországi irányítószámok és települések neveinek model class init

---
+++ virtual_receptionist/HungarianZipCodesAndCities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_receptionist
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HungarianZipCodesAndCities
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string zipCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        private string city;
+
+        /// <summary>
+        /// /
+        /// </summary>
+        public HungarianZipCodesAndCities()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ZipCode { get { return zipCode; } set { } }
+    }
+}
