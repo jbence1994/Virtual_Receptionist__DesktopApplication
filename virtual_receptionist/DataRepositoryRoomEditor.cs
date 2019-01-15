07cbee3e7c6194974687b777a3e039bfcb3e9a06
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-15 22:50:22 +0100

    UploadRoomsList() : void implementáció

--- virtual_receptionist/DataRepositoryRoomEditor.cs
+++ virtual_receptionist/DataRepositoryRoomEditor.cs
@@ -5,15 +5,51 @@ namespace virtual_receptionist.Model
 {
     public partial class DataRepository
     {
-        #region Szobanyilvántartás- és foglaláskezelő modul metódusai
+        #region Szoba kezelő modul metódusai
 
+        /// <summary>
+        /// Metódus, amely adatbázisból feltölti a szobakiadásokat tartalmazó listát
+        /// </summary>
+        /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
+        private void UploadReservationsList()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Metódus, amely adatbázisból feltölti a szobákat tartalmazó listát
+        /// </summary>
+        /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
+        private void UploadRoomsList()
+        {
+            string sql = "SELECT room.Name, room.Number, room_category.CategoryName, room.Capacity FROM room, room_category WHERE room.Category = room_category.ID ORDER BY room.Number ASC";
+            DataTable dt = database.Query(sql);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row["Name"].ToString();
+                int number = int.Parse(row["Number"].ToString());
+                string category = row["CategoryName"].ToString();
+                int capacity = int.Parse(row["Capacity"].ToString());
+
+                Room room = new Room(name, number, category, capacity);
+                rooms.Add(room);
+            }
+        }
         /// <summary>
         /// Metódus, amely visszaadja az adatbázisban tárolt összes szobakiadás adatát egy DataTable adatszerkezetben
         /// </summary>
         /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
-        public DataTable GetReservations()
+        public DataTable GetGetReservations()
         {
             throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Metódus, amely visszaadja az adatbázisban tárolt összes szoba adatát egy DataTable adatszerkezetben
+        /// </summary>
+        /// <returns>Adatokkal feltöltött DataTable-t adja vissza</returns>
+        public DataTable GetRooms()
+        {
+
         }
 
         #endregion
