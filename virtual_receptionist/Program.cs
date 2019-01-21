8ab83c242081ff6ea2d445c830aa9fd8699df46f
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-21 23:26:25 +0100

    Update Program.cs

--- virtual_receptionist/Program.cs
+++ virtual_receptionist/Program.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace virtual_receptionist.View
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
