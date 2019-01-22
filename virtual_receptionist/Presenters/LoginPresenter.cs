dbabb03f0a86603eca475bbd1f16ae9547198b17
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-22 05:33:04 +0100

    Create LoginPresenter.cs

---
+++ virtual_receptionist/Presenters/LoginPresenter.cs
@@ -0,0 +1,10 @@
+﻿using virtual_receptionist.View;
+using virtual_receptionist.Model;
+
+namespace virtual_receptionist.Presenter
+{
+    public class LoginPresenter : DefaultPresenter
+    {
+
+    }
+}
