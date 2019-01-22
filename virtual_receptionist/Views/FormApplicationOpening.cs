a7a8d949a29f804bc329e8bd35db7d447b587f8a
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-22 19:38:16 +0100

    Nyitóablak áthelyezése a Views mappába

---
+++ virtual_receptionist/Views/FormApplicationOpening.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace virtual_receptionist.View
+{
+    public partial class FormApplicationOpening : Form
+    {
+        public FormApplicationOpening()
+        {
+            InitializeComponent();
+        }
+    }
+}
