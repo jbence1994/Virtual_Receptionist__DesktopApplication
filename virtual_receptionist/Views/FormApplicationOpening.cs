0bf4c72d45313e38684f22e12be3850d76df47ef
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-22 19:42:24 +0100

    Update FormApplicationOpening.cs

--- virtual_receptionist/Views/FormApplicationOpening.cs
+++ virtual_receptionist/Views/FormApplicationOpening.cs
@@ -2,11 +2,33 @@
 
 namespace virtual_receptionist.View
 {
+    /// <summary>
+    /// Nyitóablak
+    /// </summary>
     public partial class FormApplicationOpening : Form
     {
+        #region Adattagok
+
+
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Nyitóablak konstruktora
+        /// </summary>
         public FormApplicationOpening()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region UI események
+
+        
+
+        #endregion
     }
 }
