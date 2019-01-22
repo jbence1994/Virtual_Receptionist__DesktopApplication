e6e2ce758997fd2461d8b0be679bdc4b28414a2d
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-23 00:38:27 +0100

    Szobakezelő prezenter init

---
+++ virtual_receptionist/Presenters/RoomEditorPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using virtual_receptionist.Model;
+using virtual_receptionist.View;
+
+namespace virtual_receptionist.Presenter
+{
+    /// <summary>
+    /// Szobakezelő modul prezentere
+    /// </summary>
+    public class RoomEditorPresenter : DefaultPresenter
+    {
+        #region Adattagok
+
+
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Szobakezelő modul prezenter konstruktora
+        /// </summary>
+        public RoomEditorPresenter()
+        {
+            
+        }
+
+        #endregion
+
+        #region Szobakezelő nézetfrissítései
+
+        
+
+        #endregion
+    }
+}
