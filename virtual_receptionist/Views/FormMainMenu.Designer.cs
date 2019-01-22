7a3ca1b5b34c24c540d91d5860cbee68d5093eb8
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-22 04:28:27 +0100

    Főmenü fejlesztése és GUI design

--- virtual_receptionist/Views/FormMainMenu.Designer.cs
+++ virtual_receptionist/Views/FormMainMenu.Designer.cs
@@ -30,23 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
             this.menuStripMainMenu = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemLogout = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemRoomEditor = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemGuestDatabase = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemBilling = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItemAccomodationData = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItemWebsite = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemHelpCHM = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItemAboutUs = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItemLogout = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelpSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItemAccomodationInfo = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStripMainMenu = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabelClient = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabelMenuName = new System.Windows.Forms.ToolStripStatusLabel();
             this.pictureBoxVRLogo = new System.Windows.Forms.PictureBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonGuestDatabase = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.pictureBoxBSLogo = new System.Windows.Forms.PictureBox();
             this.menuStripMainMenu.SuspendLayout();
             this.statusStripMainMenu.SuspendLayout();
@@ -58,15 +60,29 @@
             // menuStripMainMenu
             // 
             this.menuStripMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
             this.toolStripMenuItemRoomEditor,
             this.toolStripMenuItemGuestDatabase,
             this.toolStripMenuItemBilling,
-            this.toolStripMenuItemHelp,
-            this.toolStripMenuItemLogout});
+            this.toolStripMenuItemHelp});
             this.menuStripMainMenu.Location = new System.Drawing.Point(0, 0);
             this.menuStripMainMenu.Name = "menuStripMainMenu";
             this.menuStripMainMenu.Size = new System.Drawing.Size(1008, 24);
             this.menuStripMainMenu.TabIndex = 0;
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemLogout});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItemFile.Text = "Fájl";
+            // 
+            // toolStripMenuItemLogout
+            // 
+            this.toolStripMenuItemLogout.Name = "toolStripMenuItemLogout";
+            this.toolStripMenuItemLogout.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemLogout.Text = "Kijelentkezés";
             // 
             // toolStripMenuItemRoomEditor
             // 
@@ -98,56 +114,54 @@
             // toolStripMenuItemHelp
             // 
             this.toolStripMenuItemHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItemAccomodationData,
-            this.toolStripSeparator1,
+            this.toolStripMenuItemWebsite,
             this.toolStripMenuItemHelpCHM,
-            this.toolStripMenuItemAboutUs});
+            this.toolStripMenuItemAbout,
+            this.toolStripMenuItemHelpSeparator1,
+            this.toolStripMenuItemAccomodationInfo});
             this.toolStripMenuItemHelp.Name = "toolStripMenuItemHelp";
             this.toolStripMenuItemHelp.Size = new System.Drawing.Size(46, 20);
             this.toolStripMenuItemHelp.Text = "Súgó";
             this.toolStripMenuItemHelp.MouseLeave += new System.EventHandler(this.toolStripMenuItemHelp_MouseLeave);
             this.toolStripMenuItemHelp.MouseHover += new System.EventHandler(this.toolStripMenuItemHelp_MouseHover);
             // 
-            // toolStripMenuItemAccomodationData
+            // toolStripMenuItemWebsite
             // 
-            this.toolStripMenuItemAccomodationData.Name = "toolStripMenuItemAccomodationData";
-            this.toolStripMenuItemAccomodationData.Size = new System.Drawing.Size(165, 22);
-            this.toolStripMenuItemAccomodationData.Text = "Szálláshely adatai";
-            this.toolStripMenuItemAccomodationData.Click += new System.EventHandler(this.toolStripMenuItemAccomodationData_Click);
-            this.toolStripMenuItemAccomodationData.MouseLeave += new System.EventHandler(this.toolStripMenuItemAccomodationData_MouseLeave);
-            this.toolStripMenuItemAccomodationData.MouseHover += new System.EventHandler(this.toolStripMenuItemAccomodationData_MouseHover);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(162, 6);
+            this.toolStripMenuItemWebsite.Name = "toolStripMenuItemWebsite";
+            this.toolStripMenuItemWebsite.Size = new System.Drawing.Size(244, 22);
+            this.toolStripMenuItemWebsite.Text = "A Virtual Receptionist weboldala";
             // 
             // toolStripMenuItemHelpCHM
             // 
             this.toolStripMenuItemHelpCHM.Name = "toolStripMenuItemHelpCHM";
-            this.toolStripMenuItemHelpCHM.Size = new System.Drawing.Size(165, 22);
+            this.toolStripMenuItemHelpCHM.Size = new System.Drawing.Size(244, 22);
             this.toolStripMenuItemHelpCHM.Text = "Segítség";
             this.toolStripMenuItemHelpCHM.Click += new System.EventHandler(this.toolStripMenuItemHelpCHM_Click);
             this.toolStripMenuItemHelpCHM.MouseLeave += new System.EventHandler(this.toolStripMenuItemHelpCHM_MouseLeave);
             this.toolStripMenuItemHelpCHM.MouseHover += new System.EventHandler(this.toolStripMenuItemHelpCHM_MouseHover);
             // 
-            // toolStripMenuItemAboutUs
+            // toolStripMenuItemAbout
             // 
-            this.toolStripMenuItemAboutUs.Name = "toolStripMenuItemAboutUs";
-            this.toolStripMenuItemAboutUs.Size = new System.Drawing.Size(165, 22);
-            this.toolStripMenuItemAboutUs.Text = "Névjegy";
-            this.toolStripMenuItemAboutUs.Click += new System.EventHandler(this.toolStripMenuItemAboutUs_Click);
-            this.toolStripMenuItemAboutUs.MouseLeave += new System.EventHandler(this.toolStripMenuItemAboutUs_MouseLeave);
-            this.toolStripMenuItemAboutUs.MouseHover += new System.EventHandler(this.toolStripMenuItemAboutUs_MouseHover);
+            this.toolStripMenuItemAbout.Name = "toolStripMenuItemAbout";
+            this.toolStripMenuItemAbout.Size = new System.Drawing.Size(244, 22);
+            this.toolStripMenuItemAbout.Text = "Névjegy";
+            this.toolStripMenuItemAbout.Click += new System.EventHandler(this.toolStripMenuItemAboutUs_Click);
+            this.toolStripMenuItemAbout.MouseLeave += new System.EventHandler(this.toolStripMenuItemAboutUs_MouseLeave);
+            this.toolStripMenuItemAbout.MouseHover += new System.EventHandler(this.toolStripMenuItemAboutUs_MouseHover);
             // 
-            // toolStripMenuItemLogout
+            // toolStripMenuItemHelpSeparator1
             // 
-            this.toolStripMenuItemLogout.Name = "toolStripMenuItemLogout";
-            this.toolStripMenuItemLogout.Size = new System.Drawing.Size(86, 20);
-            this.toolStripMenuItemLogout.Text = "Kijelentkezés";
-            this.toolStripMenuItemLogout.Click += new System.EventHandler(this.toolStripMenuItemLogout_Click);
-            this.toolStripMenuItemLogout.MouseLeave += new System.EventHandler(this.toolStripMenuItemQuit_MouseLeave);
-            this.toolStripMenuItemLogout.MouseHover += new System.EventHandler(this.toolStripMenuItemQuit_MouseHover);
+            this.toolStripMenuItemHelpSeparator1.Name = "toolStripMenuItemHelpSeparator1";
+            this.toolStripMenuItemHelpSeparator1.Size = new System.Drawing.Size(241, 6);
+            // 
+            // toolStripMenuItemAccomodationInfo
+            // 
+            this.toolStripMenuItemAccomodationInfo.Name = "toolStripMenuItemAccomodationInfo";
+            this.toolStripMenuItemAccomodationInfo.Size = new System.Drawing.Size(244, 22);
+            this.toolStripMenuItemAccomodationInfo.Text = "Szálláshely adatai";
+            this.toolStripMenuItemAccomodationInfo.Click += new System.EventHandler(this.toolStripMenuItemAccomodationData_Click);
+            this.toolStripMenuItemAccomodationInfo.MouseLeave += new System.EventHandler(this.toolStripMenuItemAccomodationData_MouseLeave);
+            this.toolStripMenuItemAccomodationInfo.MouseHover += new System.EventHandler(this.toolStripMenuItemAccomodationData_MouseHover);
             // 
             // statusStripMainMenu
             // 
@@ -173,9 +187,9 @@
             // 
             // pictureBoxVRLogo
             // 
-            this.pictureBoxVRLogo.Location = new System.Drawing.Point(12, 52);
+            this.pictureBoxVRLogo.Location = new System.Drawing.Point(12, 66);
             this.pictureBoxVRLogo.Name = "pictureBoxVRLogo";
-            this.pictureBoxVRLogo.Size = new System.Drawing.Size(326, 450);
+            this.pictureBoxVRLogo.Size = new System.Drawing.Size(326, 436);
             this.pictureBoxVRLogo.TabIndex = 5;
             this.pictureBoxVRLogo.TabStop = false;
             // 
@@ -183,11 +197,11 @@
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3});
+            this.toolStripButtonGuestDatabase,
+            this.toolStripButton2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1008, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1008, 39);
             this.toolStrip1.TabIndex = 6;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -195,28 +209,32 @@
             // 
             this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton1.Text = "Szobakezelő";
+            // 
+            // toolStripButtonGuestDatabase
+            // 
+            this.toolStripButtonGuestDatabase.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonGuestDatabase.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonGuestDatabase.Image")));
+            this.toolStripButtonGuestDatabase.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButtonGuestDatabase.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonGuestDatabase.Name = "toolStripButtonGuestDatabase";
+            this.toolStripButtonGuestDatabase.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButtonGuestDatabase.Text = "Vendégadatbázis";
             // 
             // toolStripButton2
             // 
             this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
-            // 
-            // toolStripButton3
-            // 
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripButton2.Size = new System.Drawing.Size(28, 36);
+            this.toolStripButton2.Text = "Számlázás";
             // 
             // pictureBoxBSLogo
             // 
@@ -269,13 +287,15 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButtonGuestDatabase;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelMenuName;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelpCHM;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAboutUs;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemLogout;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAccomodationData;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAbout;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAccomodationInfo;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItemHelpSeparator1;
         private System.Windows.Forms.PictureBox pictureBoxBSLogo;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemWebsite;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemLogout;
     }
 }
