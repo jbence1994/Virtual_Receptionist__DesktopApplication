58dc5c44ca5848259112ac080d109902a214139b
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-22 21:48:00 +0100

    Nyitóablak prezenter init

---
+++ virtual_receptionist/Presenters/ApplicationOpeningPresenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace virtual_receptionist.Presenter
+{
+    /// <summary>
+    /// Nyitóablak prezentere
+    /// </summary>
+    public class ApplicationOpeningPresenter : DefaultPresenter
+    {
+        #region Adattagok
+
+
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Nyitóablak prezenter konstruktora
+        /// </summary>
+        public ApplicationOpeningPresenter()
+        {
+
+        }
+
+        #endregion
+
+        #region UI események
+
+        
+
+        #endregion
+    }
+}
