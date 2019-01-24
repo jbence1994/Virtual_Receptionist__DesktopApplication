6bd176ddadd73f38cdfa8ea921857f1691b29485
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-24 18:04:40 +0100

    Update ModalBillingPresenter.cs

--- virtual_receptionist/Presenters/ModalBillingPresenter.cs
+++ virtual_receptionist/Presenters/ModalBillingPresenter.cs
@@ -3,8 +3,33 @@ using virtual_receptionist.Model;
 
 namespace virtual_receptionist.Presenter
 {
+    /// <summary>
+    /// Számlázó modul ügyfél adatait kitöltő modális ablak prezentere
+    /// </summary>
     public class ModalBillingPresenter : DefaultPresenter
     {
+        #region Adattagok
 
+
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Számlázó modul ügyfél adatait kitöltő modális ablak prezenter konsturktora
+        /// </summary>
+        public ModalBillingPresenter()
+        {
+
+        }
+
+        #endregion
+
+        #region Ügyfél adatait kitöltő modális ablak nézetfrissítései
+
+
+
+        #endregion
     }
 }
