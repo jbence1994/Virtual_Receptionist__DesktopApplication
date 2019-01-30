48ff0351ae465428b07e4956cad625029a23ad7e
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-30 05:22:12 +0100

    Update DefaultPresenter.cs

--- virtual_receptionist/Presenters/DefaultPresenter.cs
+++ virtual_receptionist/Presenters/DefaultPresenter.cs
@@ -37,14 +37,7 @@ namespace virtual_receptionist.Presenter
         /// <param name="form">Ablak, amely bezárásra kerül</param>
         public void BackToMainMenu(Form form)
         {
-            DialogResult backToMainMenu =
-                MessageBox.Show("Nem mentett változásai vannak! Biztosan visszalép a főmenübe?", "",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-            if (backToMainMenu == DialogResult.Yes)
-            {
-                form.Close();
-            }
+            form.Close();
         }
 
         /// <summary>
