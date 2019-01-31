d03153be43870dd92f36ea14318d7dd68ecabbd8
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-31 18:43:19 +0100

    CRUD metódusok és DataTable inicializáló metódusok refaktor ID mezővel

--- virtual_receptionist/Models/DataRepositoryGuest.cs
+++ virtual_receptionist/Models/DataRepositoryGuest.cs
@@ -16,11 +16,12 @@ namespace virtual_receptionist.Model
         private void UploadPrivateGuestsList()
         {
             string sql =
-                "SELECT guest.Name, guest.DocumentNumber, guest.Citizenship, guest.BirthDate, country.CountryName, guest.ZipCode, guest.City, guest.Address, guest.VATNumber, guest.PhoneNumber, guest.EmailAddress FROM guest, country WHERE guest.Country = country.ID AND guest.DocumentNumber != \"\"";
+                "SELECT guest.ID, guest.Name, guest.DocumentNumber, guest.Citizenship, guest.BirthDate, country.CountryName, guest.ZipCode, guest.City, guest.Address, guest.VATNumber, guest.PhoneNumber, guest.EmailAddress FROM guest, country WHERE guest.Country = country.ID AND guest.DocumentNumber != \"\"";
             DataTable dt = database.DQL(sql);
 
             foreach (DataRow row in dt.Rows)
             {
+                int id = int.Parse(row["ID"].ToString());
                 string name = row["Name"].ToString();
                 string documentNumber = row["DocumentNumber"].ToString();
                 string citizenship = row["Citizenship"].ToString();
@@ -32,7 +33,7 @@ namespace virtual_receptionist.Model
                 string phoneNumber = row["PhoneNumber"].ToString();
                 string emailAddress = row["EmailAddress"].ToString();
 
-                PrivateGuest privateGuestInstance = new PrivateGuest(name, documentNumber, citizenship, birthDate,
+                PrivateGuest privateGuestInstance = new PrivateGuest(id, name, documentNumber, citizenship, birthDate,
                     country, zipCode, city, address, phoneNumber, emailAddress);
 
                 privateGuests.Add(privateGuestInstance);
@@ -50,6 +51,7 @@ namespace virtual_receptionist.Model
 
             foreach (DataRow row in dt.Rows)
             {
+                int id = int.Parse(row["ID"].ToString());
                 string name = row["Name"].ToString();
                 string vatNumber = row["VATNumber"].ToString();
                 string country = row["CountryName"].ToString();
@@ -59,7 +61,7 @@ namespace virtual_receptionist.Model
                 string phoneNumber = row["PhoneNumber"].ToString();
                 string emailAddress = row["EmailAddress"].ToString();
 
-                CorporateGuest corporateGuestInstance = new CorporateGuest(name, vatNumber, country, zipCode, city,
+                CorporateGuest corporateGuestInstance = new CorporateGuest(id, name, vatNumber, country, zipCode, city,
                     address, phoneNumber, emailAddress);
 
                 corporateGuests.Add(corporateGuestInstance);
@@ -78,6 +80,7 @@ namespace virtual_receptionist.Model
             }
 
             DataTable privateGuestsDataTable = new DataTable();
+            privateGuestsDataTable.Columns.Add("ID", typeof(int));
             privateGuestsDataTable.Columns.Add("Name", typeof(string));
             privateGuestsDataTable.Columns.Add("DocumentNumber", typeof(string));
             privateGuestsDataTable.Columns.Add("Citizenship", typeof(string));
@@ -91,7 +94,7 @@ namespace virtual_receptionist.Model
 
             foreach (PrivateGuest privateGuest in privateGuests)
             {
-                privateGuestsDataTable.Rows.Add(privateGuest.Name, privateGuest.DocumentNumber,
+                privateGuestsDataTable.Rows.Add(privateGuest.ID, privateGuest.Name, privateGuest.DocumentNumber,
                     privateGuest.Citizenship,
                     privateGuest.BirthDate, privateGuest.Country, privateGuest.ZipCode, privateGuest.City,
                     privateGuest.Address, privateGuest.PhoneNumber, privateGuest.EmailAddress);
@@ -112,6 +115,7 @@ namespace virtual_receptionist.Model
             }
 
             DataTable corporateGuestDataTable = new DataTable();
+            corporateGuestDataTable.Columns.Add("ID", typeof(int));
             corporateGuestDataTable.Columns.Add("Name", typeof(string));
             corporateGuestDataTable.Columns.Add("VATNumber", typeof(string));
             corporateGuestDataTable.Columns.Add("Country", typeof(string));
@@ -123,7 +127,7 @@ namespace virtual_receptionist.Model
 
             foreach (CorporateGuest corporateGuest in corporateGuests)
             {
-                corporateGuestDataTable.Rows.Add(corporateGuest.Name, corporateGuest.VatNumber,
+                corporateGuestDataTable.Rows.Add(corporateGuest.ID, corporateGuest.Name, corporateGuest.VatNumber,
                     corporateGuest.Country, corporateGuest.ZipCode, corporateGuest.City, corporateGuest.Address,
                     corporateGuest.PhoneNumber, corporateGuest.EmailAddress);
             }
@@ -137,7 +141,7 @@ namespace virtual_receptionist.Model
         /// <param name="privateGuest">PrivateGuest objektum</param>
         public void DeleteGuest(PrivateGuest privateGuest)
         {
-            string sql = $"DELETE FROM guest WHERE guest.Name LIKE \"{privateGuest.Name}\"";
+            string sql = $"DELETE FROM guest WHERE guest.ID LIKE \"{privateGuest.ID}\"";
             database.DML(sql);
         }
 
@@ -147,7 +151,7 @@ namespace virtual_receptionist.Model
         /// <param name="corporateGuest">CorporateGuest objektum</param>
         public void DeleteGuest(CorporateGuest corporateGuest)
         {
-            string sql = $"DELETE FROM guest WHERE guest.Name LIKE \"{corporateGuest.Name}\"";
+            string sql = $"DELETE FROM guest WHERE guest.ID LIKE \"{corporateGuest.ID}\"";
             database.DML(sql);
         }
 
@@ -158,7 +162,7 @@ namespace virtual_receptionist.Model
         public void UpdateGuest(PrivateGuest privateGuest)
         {
             string sql =
-                $"UPDATE guest SET guest.Name=\"{privateGuest.Name}\", guest.DocumentNumber=\"{privateGuest.DocumentNumber}\", guest.Citizenship=\"{privateGuest.Citizenship}\", guest.BirthDate=\"{privateGuest.BirthDate}\", guest.Country=\"{privateGuest.Country}\", guest.ZipCode=\"{privateGuest.ZipCode}\", guest.City=\"{privateGuest.City}\", guest.Address=\"{privateGuest.Address}\", guest.PhoneNumber=\"{privateGuest.PhoneNumber}\", guest.EmailAddress=\"{privateGuest.EmailAddress}\" WHERE guest.Name LIKE \"{privateGuest.Name}\"";
+                $"UPDATE guest SET guest.ID=\"{privateGuest.ID}\", guest.Name=\"{privateGuest.Name}\", guest.DocumentNumber=\"{privateGuest.DocumentNumber}\", guest.Citizenship=\"{privateGuest.Citizenship}\", guest.BirthDate=\"{privateGuest.BirthDate}\", guest.Country=\"{privateGuest.Country}\", guest.ZipCode=\"{privateGuest.ZipCode}\", guest.City=\"{privateGuest.City}\", guest.Address=\"{privateGuest.Address}\", guest.PhoneNumber=\"{privateGuest.PhoneNumber}\", guest.EmailAddress=\"{privateGuest.EmailAddress}\" WHERE guest.Name LIKE \"{privateGuest.Name}\"";
             database.DML(sql);
         }
 
@@ -169,7 +173,7 @@ namespace virtual_receptionist.Model
         public void UpdateGuest(CorporateGuest corporateGuest)
         {
             string sql =
-                $"UPDATE guest SET guest.Name=\"{corporateGuest.Name}\", guest.VATNumber=\"{corporateGuest.VatNumber}\", guest.Country=\"{corporateGuest.Country}\", guest.ZipCode=\"{corporateGuest.ZipCode}\", guest.City=\"{corporateGuest.City}\", guest.Address=\"{corporateGuest.Address}\", guest.PhoneNumber=\"{corporateGuest.PhoneNumber}\", guest.EmailAddress=\"{corporateGuest.EmailAddress}\" WHERE guest.Name LIKE \"{corporateGuest.Name}\"";
+                $"UPDATE guest SET guest.ID=\"{corporateGuest.ID}\", guest.Name=\"{corporateGuest.Name}\", guest.VATNumber=\"{corporateGuest.VatNumber}\", guest.Country=\"{corporateGuest.Country}\", guest.ZipCode=\"{corporateGuest.ZipCode}\", guest.City=\"{corporateGuest.City}\", guest.Address=\"{corporateGuest.Address}\", guest.PhoneNumber=\"{corporateGuest.PhoneNumber}\", guest.EmailAddress=\"{corporateGuest.EmailAddress}\" WHERE guest.Name LIKE \"{corporateGuest.Name}\"";
             database.DML(sql);
         }
 
@@ -180,7 +184,7 @@ namespace virtual_receptionist.Model
         public void CreateGuest(PrivateGuest privateGuest)
         {
             string sql =
-                $"INSERT INTO guest(Name, DocumentNumber, Citizenship, BirthDate, Country, ZipCode, City, Address, PhoneNumber, EmailAddress) VALUES(\"{privateGuest.Name}\", \"{privateGuest.DocumentNumber}\", \"{privateGuest.Citizenship}\", \"{privateGuest.BirthDate}\", (SELECT Country.ID FROM Country WHERE Country.CountryName LIKE \"{privateGuest.Country}\"), \"{privateGuest.ZipCode}\", \"{privateGuest.City}\", \"{privateGuest.Address}\", \"{privateGuest.PhoneNumber}\", \"{privateGuest.EmailAddress}\")";
+                $"INSERT INTO guest(ID, Name, DocumentNumber, Citizenship, BirthDate, Country, ZipCode, City, Address, PhoneNumber, EmailAddress) VALUES(\"{privateGuest.ID}\",\"{privateGuest.Name}\", \"{privateGuest.DocumentNumber}\", \"{privateGuest.Citizenship}\", \"{privateGuest.BirthDate}\", (SELECT Country.ID FROM Country WHERE Country.CountryName LIKE \"{privateGuest.Country}\"), \"{privateGuest.ZipCode}\", \"{privateGuest.City}\", \"{privateGuest.Address}\", \"{privateGuest.PhoneNumber}\", \"{privateGuest.EmailAddress}\")";
             database.DML(sql);
         }
 
@@ -191,7 +195,7 @@ namespace virtual_receptionist.Model
         public void CreateGuest(CorporateGuest corporateGuest)
         {
             string sql =
-                $"INSERT INTO guest(Name, VATNumber, Country, ZipCode, City, Address, PhoneNumber, EmailAddress) VALUES(\"{corporateGuest.Name}\", \"{corporateGuest.VatNumber}\", (SELECT Country.ID FROM Country WHERE Country.CountryName LIKE \"{corporateGuest.Country}\"), \"{corporateGuest.ZipCode}\", \"{corporateGuest.City}\", \"{corporateGuest.Address}\", \"{corporateGuest.PhoneNumber}\", \"{corporateGuest.EmailAddress}\")";
+                $"INSERT INTO guest(ID, Name, VATNumber, Country, ZipCode, City, Address, PhoneNumber, EmailAddress) VALUES(\"{corporateGuest.ID}\",\"{corporateGuest.Name}\", \"{corporateGuest.VatNumber}\", (SELECT Country.ID FROM Country WHERE Country.CountryName LIKE \"{corporateGuest.Country}\"), \"{corporateGuest.ZipCode}\", \"{corporateGuest.City}\", \"{corporateGuest.Address}\", \"{corporateGuest.PhoneNumber}\", \"{corporateGuest.EmailAddress}\")";
 
             Debug.WriteLine(sql);
 
