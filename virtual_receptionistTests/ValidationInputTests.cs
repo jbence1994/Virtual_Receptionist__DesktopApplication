3b3e4e4dc1efbaed452d965349a394997475aa53
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-07 16:07:27 +0100

    Első input teszt névre

--- virtual_receptionistTests/ValidationInputTests.cs
+++ virtual_receptionistTests/ValidationInputTests.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using virtual_receptionist.Exceptions;
 
 namespace virtual_receptionist.Validation.ValidationTests
 {
     [TestClass()]
     public class ValidationInputTests
     {
+        /// <summary>
+        /// Tesztmetódus, amely ellenőrzi, üres-e a bemeneti név
+        /// </summary>
         [TestMethod()]
-        public void ProvideNameTest()
+        public void ProvideNameTest_InCaseInputNameIsEmpty()
         {
-            Assert.Fail();
+            try
+            {
+                Input inputName = new Input("");
+                inputName.ProvideName();
+                Assert.Fail("Nem dobott kivételt üres névre!");
+            }
+            catch (InvalidNameException)
+            {
+            }
         }
     }
 }
