ebffb168b898b917f8a45b6d95a93e92dac0d5e1
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-07 19:01:08 +0100

    Unit test rename

--- virtual_receptionistTests/Properties/AssemblyInfo.cs
+++ virtual_receptionistTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("virtual_receptionistValidationInputTests")]
+[assembly: AssemblyTitle("virtual_receptionistInputValidationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("virtual_receptionistValidationInputTests")]
+[assembly: AssemblyProduct("virtual_receptionistInputValidationTests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
