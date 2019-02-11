3afafeb5732522130c89c1d8e9e2019103caf705
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-11 01:00:17 +0100

    MySQL connector update. Kód refaktor (params object[] guestParameters)

--- virtual_receptionist/Presenter/GuestDatabasePresenter.cs
+++ virtual_receptionist/Presenter/GuestDatabasePresenter.cs
@@ -43,9 +43,9 @@ namespace virtual_receptionist.Presenter
         }
 
         /// <summary>
-        /// 
+        /// Országokat lista adatszerkezetben visszaadó metódus
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Országlistával tér vissza a függvény</returns>
         public List<Country> GetCountries()
         {
             List<Country> countries = dataRepository.GetCountries();
@@ -53,9 +53,9 @@ namespace virtual_receptionist.Presenter
         }
 
         /// <summary>
-        /// 
+        /// Vendég adatbázis tábla soron következő azonosítóját visszaadó metódus
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Soron következő vendégazonosítóval tér vissza a függvény</returns>
         public int GetNextID()
         {
             int nextID = 1;
@@ -71,13 +71,24 @@ namespace virtual_receptionist.Presenter
         /// <summary>
         /// Vendégtáblázatba és adatbázisba új rekord félvetélére szolgáló metódus
         /// </summary>
-        public void AddNewRecordToPrivateGuestTable(int id, string name, string documentNumber, string citizenship,
-            string birthDate, string country, string zipCode, string city, string address, string
-                phoneNumber, string email)
+        /// <param name="guestParameters">Vendég pataméterei</param>
+        public void AddNewRecordToPrivateGuestTable(params object[] guestParameters)
         {
-            PrivateGuest privateGuest = new PrivateGuest(id, name, documentNumber, citizenship, birthDate, country,
-                zipCode,
-                city, address, phoneNumber, email);
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string documentNumber = guestParameters[2].ToString();
+            string citizenship = guestParameters[3].ToString();
+            string birthDate = guestParameters[4].ToString();
+            string country = guestParameters[5].ToString();
+            string zipCode = guestParameters[6].ToString();
+            string city = guestParameters[7].ToString();
+            string address = guestParameters[8].ToString();
+            string phoneNumber = guestParameters[9].ToString();
+            string email = guestParameters[10].ToString();
+
+            PrivateGuest privateGuest =
+                new PrivateGuest(id, name, documentNumber, citizenship, birthDate, country, zipCode, city, address,
+                    phoneNumber, email);
 
             dataRepository.CreateGuest(privateGuest);
         }
@@ -85,64 +96,118 @@ namespace virtual_receptionist.Presenter
         /// <summary>
         /// Céges vendégtáblázatba és adatbázisba új rekord félvetélére szolgáló metódus
         /// </summary>
-        public void AddNewRecordToCorporateGuestTable(int id, string name, string vatNumber, string country,
-            string zipCode, string city, string address, string phoneNumber, string email)
+        /// <param name="guestParameters">Céges vendég paraméterei</param>
+        public void AddNewRecordToCorporateGuestTable(params object[] guestParameters)
         {
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string country = guestParameters[2].ToString();
+            string vatNumber = guestParameters[3].ToString();
+            string zipCode = guestParameters[4].ToString();
+            string city = guestParameters[5].ToString();
+            string address = guestParameters[6].ToString();
+            string phoneNumber = guestParameters[7].ToString();
+            string email = guestParameters[8].ToString();
+
             CorporateGuest corporateGuest =
                 new CorporateGuest(id, name, vatNumber, country, zipCode, city, address, phoneNumber, email);
+
             dataRepository.CreateGuest(corporateGuest);
         }
 
         /// <summary>
         /// Vendégtáblázatban és adatbázisban meglévő rekord módosítására szolgáló metódus
         /// </summary>
-        public void UpdateRecordInPrivateGuestTable(int id, string name, string documentNumber, string citizenship,
-            string birthDate, string country,
-            string zipCode,
-            string city, string address, string phoneNumber, string email)
+        /// <param name="guestParameters">Vendég paraméterei</param>
+        public void UpdateRecordInPrivateGuestTable(params object[] guestParameters)
         {
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string documentNumber = guestParameters[2].ToString();
+            string citizenship = guestParameters[3].ToString();
+            string birthDate = guestParameters[4].ToString();
+            string country = guestParameters[5].ToString();
+            string zipCode = guestParameters[6].ToString();
+            string city = guestParameters[7].ToString();
+            string address = guestParameters[8].ToString();
+            string phoneNumber = guestParameters[9].ToString();
+            string email = guestParameters[10].ToString();
+
             PrivateGuest privateGuest = new PrivateGuest(id, name, documentNumber, citizenship, birthDate, country,
                 zipCode,
                 city, address, phoneNumber, email);
+
             dataRepository.UpdateGuest(privateGuest);
         }
 
         /// <summary>
         /// Céges vendégtáblázatban és adatbázisban meglévő rekord módosítására szolgáló metódus
         /// </summary>
-        public void UpdateRecordInCorporateGuestTable(int id, string name, string vatNumber, string country,
-            string zipCode, string city, string address, string phoneNumber, string email)
+        /// <param name="guestParameters">Céges vendég paraméterei</param>
+        public void UpdateRecordInCorporateGuestTable(params object[] guestParameters)
         {
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string country = guestParameters[2].ToString();
+            string vatNumber = guestParameters[3].ToString();
+            string zipCode = guestParameters[4].ToString();
+            string city = guestParameters[5].ToString();
+            string address = guestParameters[6].ToString();
+            string phoneNumber = guestParameters[7].ToString();
+            string email = guestParameters[8].ToString();
+
             CorporateGuest corporateGuest =
                 new CorporateGuest(id, name,
                     vatNumber, country, zipCode, city, address, phoneNumber, email);
+
             dataRepository.UpdateGuest(corporateGuest);
         }
 
         /// <summary>
         /// Vendégtáblázatból és adatbázisból meglévő rekord törlésére szolgáló metódus
         /// </summary>
-        public void DeleteRecordFromPrivateGuestTable(int id, string name, string documentNumber, string citizenship,
-            string birthDate, string country, string zipCode, string city,
-            string address, string phoneNumber, string email)
+        /// <param name="guestParameters">Vendég paraméterei</param>
+        public void DeleteRecordFromPrivateGuestTable(params object[] guestParameters)
         {
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string documentNumber = guestParameters[2].ToString();
+            string citizenship = guestParameters[3].ToString();
+            string birthDate = guestParameters[4].ToString();
+            string country = guestParameters[5].ToString();
+            string zipCode = guestParameters[6].ToString();
+            string city = guestParameters[7].ToString();
+            string address = guestParameters[8].ToString();
+            string phoneNumber = guestParameters[9].ToString();
+            string email = guestParameters[10].ToString();
+
             PrivateGuest privateGuest = new PrivateGuest(id, name, documentNumber, citizenship, birthDate,
                 country,
                 zipCode, city, address, phoneNumber, email);
+
             dataRepository.DeleteGuest(privateGuest);
         }
 
         /// <summary>
         /// Céges vendégtáblázatból és adatbázisból meglévő rekord törlésére szolgáló metódus
         /// </summary>
-        public void DeleteRecordFromCorporateGuestTable(int id, string name, string vatNumber, string country,
-            string zipCode, string city, string
-                address, string
-                phoneNumber, string email)
+        /// <param name="guestParameters">Céges vendég paraméterei</param>
+        public void DeleteRecordFromCorporateGuestTable(params object[] guestParameters)
         {
+            int id = int.Parse(guestParameters[0].ToString());
+            string name = guestParameters[1].ToString();
+            string country = guestParameters[2].ToString();
+            string vatNumber = guestParameters[3].ToString();
+            string zipCode = guestParameters[4].ToString();
+            string city = guestParameters[5].ToString();
+            string address = guestParameters[6].ToString();
+            string phoneNumber = guestParameters[7].ToString();
+            string email = guestParameters[8].ToString();
+
             CorporateGuest corporateGuest = new CorporateGuest(id, name, vatNumber, country, zipCode, city,
                 address,
                 phoneNumber, email);
+
             dataRepository.DeleteGuest(corporateGuest);
         }
 
