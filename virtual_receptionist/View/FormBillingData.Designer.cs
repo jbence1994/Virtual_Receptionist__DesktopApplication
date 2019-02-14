c4d3648d598f7f7139b576f02d6f26612fdb5c01
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-14 21:58:40 +0100

    ListView UI redesign

--- virtual_receptionist/View/FormBillingData.Designer.cs
+++ virtual_receptionist/View/FormBillingData.Designer.cs
@@ -53,7 +53,7 @@
             this.listViewPrivateGuests.GridLines = true;
             this.listViewPrivateGuests.Location = new System.Drawing.Point(12, 12);
             this.listViewPrivateGuests.Name = "listViewPrivateGuests";
-            this.listViewPrivateGuests.Size = new System.Drawing.Size(776, 426);
+            this.listViewPrivateGuests.Size = new System.Drawing.Size(776, 251);
             this.listViewPrivateGuests.TabIndex = 2;
             this.listViewPrivateGuests.UseCompatibleStateImageBehavior = false;
             this.listViewPrivateGuests.View = System.Windows.Forms.View.Details;
@@ -61,43 +61,43 @@
             // columnBookingID
             // 
             this.columnBookingID.Text = "Foglalás azonosító";
-            this.columnBookingID.Width = 110;
+            this.columnBookingID.Width = 150;
             // 
             // columnHeaderName
             // 
             this.columnHeaderName.Text = "Név";
-            this.columnHeaderName.Width = 110;
+            this.columnHeaderName.Width = 150;
             // 
             // columnHeaderRoomNumber
             // 
             this.columnHeaderRoomNumber.Text = "Szobaszám";
-            this.columnHeaderRoomNumber.Width = 110;
+            this.columnHeaderRoomNumber.Width = 150;
             // 
             // columnHeaderRoomCategory
             // 
             this.columnHeaderRoomCategory.Text = "Szoba neve";
-            this.columnHeaderRoomCategory.Width = 110;
+            this.columnHeaderRoomCategory.Width = 150;
             // 
             // columnHeaderNumberOfGuests
             // 
             this.columnHeaderNumberOfGuests.Text = "Vendégek száma";
-            this.columnHeaderNumberOfGuests.Width = 110;
+            this.columnHeaderNumberOfGuests.Width = 150;
             // 
             // columnHeaderArrivalDate
             // 
             this.columnHeaderArrivalDate.Text = "Érkezés";
-            this.columnHeaderArrivalDate.Width = 110;
+            this.columnHeaderArrivalDate.Width = 150;
             // 
             // columnHeaderDepartureDate
             // 
             this.columnHeaderDepartureDate.Text = "Távozás";
-            this.columnHeaderDepartureDate.Width = 110;
+            this.columnHeaderDepartureDate.Width = 150;
             // 
             // FormBillingData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 453);
+            this.ClientSize = new System.Drawing.Size(802, 275);
             this.Controls.Add(this.listViewPrivateGuests);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormBillingData";
