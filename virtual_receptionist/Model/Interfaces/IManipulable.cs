f221b0c13a6033f84cc50d7d0a90f26f762b83bd
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-22 20:22:45 +0100

    CRUD interfész létrehozása.

---
+++ virtual_receptionist/Model/Interfaces/IManipulable.cs
@@ -0,0 +1,29 @@
+﻿namespace virtual_receptionist.Model.Interfaces
+{
+    /// <summary>
+    /// CRUD metódusokat leíró interfész
+    /// </summary>
+    public interface IManipulable
+    {
+        /// <summary>
+        /// Új elem létrehozása
+        /// </summary>
+        /// <typeparam name="T">Típus</typeparam>
+        /// <param name="item">Típusú elem</param>
+        void Create<T>(T item) where T : class;
+
+        /// <summary>
+        /// Meglévő elem módosítása
+        /// </summary>
+        /// <typeparam name="T">Típus</typeparam>
+        /// <param name="item">Típusú elem</param>
+        void Update<T>(T item) where T : class;
+
+        /// <summary>
+        /// Meglévő elem törlése
+        /// </summary>
+        /// <typeparam name="T">Típus</typeparam>
+        /// <param name="item">Típusú elem</param>
+        void Delete<T>(T item) where T : class;
+    }
+}
