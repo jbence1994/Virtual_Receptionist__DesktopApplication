cbc9536cd293964abcd76ea9db2e9ec9b5424674
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-03-05 00:04:17 +0100

    Adatbázis frissítési lehetőség.

--- virtual_receptionist/Views/FormGuestDatabase.cs
+++ virtual_receptionist/Views/FormGuestDatabase.cs
@@ -186,6 +186,11 @@ namespace virtual_receptionist.Views
             }
         }
 
+        private void buttonRefreshDatabase_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void listViewGuest_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (listViewGuest.SelectedItems.Count > 0)
