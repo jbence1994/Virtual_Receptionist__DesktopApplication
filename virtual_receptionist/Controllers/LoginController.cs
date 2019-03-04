dea249f1e374f083ff32a9febb2da6f8f152359f
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-03-04 23:18:01 +0100

    Update LoginController.cs

--- virtual_receptionist/Controllers/LoginController.cs
+++ virtual_receptionist/Controllers/LoginController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using virtual_receptionist.Controllers.Exceptions;
+﻿using virtual_receptionist.Controllers.Exceptions;
 using virtual_receptionist.Models.Data;
 using virtual_receptionist.Models.DatabaseConnection;
 
