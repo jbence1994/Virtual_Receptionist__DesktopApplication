85b12840ea74a50edb75cf6e1cbde5dd924d20af
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-03-05 16:00:08 +0100

    Update BookingRepository.cs

--- virtual_receptionist/Models/Data/BookingRepository.cs
+++ virtual_receptionist/Models/Data/BookingRepository.cs
@@ -136,7 +136,7 @@ namespace virtual_receptionist.Models.Data
         public void Create(Booking booking)
         {
             string sql =
-                $"INSERT INTO booking(GuestID, RoomID, NumberOfGuests, ArrivalDate, DepartureDate) VALUES ((SELECT guest.ID FROM guest WHERE guest.Name LIKE \"{booking.Guest.Name}\"), (SELECT room.ID FROM room WHERE room.Number = \"{booking.Room.Number}\"), \"{booking.NumberOfGuests}\", {booking.ArrivalDate}, {booking.DepartureDate});";
+                $"INSERT INTO booking(GuestID, RoomID, NumberOfGuests, ArrivalDate, DepartureDate) VALUES ((SELECT guest.ID FROM guest WHERE guest.Name LIKE \"{booking.Guest.Name}\"), (SELECT room.ID FROM room WHERE room.Number = \"{booking.Room.Number}\"), \"{booking.NumberOfGuests}\", \"{booking.ArrivalDate}\", \"{booking.DepartureDate}\");";
             database.DML(sql);
         }
 
@@ -157,7 +157,7 @@ namespace virtual_receptionist.Models.Data
         public void Update(Booking booking)
         {
             string sql =
-                $"UPDATE room SET booking.GuestID = (SELECT guest.Name FROM guest WHERE guest.Name = \"{booking.Guest.Name}\"), booking.RoomID = (SELECT room.Name FROM room WHERE room.ID = \"{booking.Room.Name}\"), NumberOfGuests = \"{booking.NumberOfGuests}\", ArrivalDate = {booking.ArrivalDate}, DepartureDate = {booking.DepartureDate} WHERE booking.ID = \"{booking.ID}\"";
+                $"UPDATE room SET booking.GuestID = (SELECT guest.Name FROM guest WHERE guest.Name = \"{booking.Guest.Name}\"), booking.RoomID = (SELECT room.Name FROM room WHERE room.ID = \"{booking.Room.Name}\"), NumberOfGuests = \"{booking.NumberOfGuests}\", ArrivalDate = \"{booking.ArrivalDate}\", DepartureDate = \"{booking.DepartureDate}\" WHERE booking.ID = \"{booking.ID}\"";
             database.DML(sql);
         }
 
