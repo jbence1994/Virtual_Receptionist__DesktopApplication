78259b8f922a5c7093ed35c7ef6c4bf93873a64e
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-03-14 00:48:26 +0100

    Foglalás ellenőrző osztály.

---
+++ virtual_receptionist/Controllers/Validation/BookingValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using virtual_receptionist.Controllers.Exceptions;
+using static virtual_receptionist.Controllers.Validation.InputValidation;
+
+namespace virtual_receptionist.Controllers.Validation
+{
+    /// <summary>
+    /// Foglalás adatait ellenőrző osztály
+    /// </summary>
+    public class BookingValidation
+    {
+        #region Adattagok
+
+        #endregion
+
+        #region Konstruktor
+
+        public BookingValidation()
+        {
+
+        }
+
+        #endregion
+
+        #region Metódusok
+
+        /// <summary>
+        /// Foglalás ellenőrző metódus
+        /// </summary>
+        /// <exception cref="InvalidBookingException"></exception>
+        public void ValidateBooking()
+        {
+
+        }
+
+        #endregion
+    }
+}
