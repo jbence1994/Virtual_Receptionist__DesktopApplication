f40343cecd49a2c1bde9b8097f789c2f69b7cae6
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-03-14 00:32:26 +0100

    foglalással összefüggő kivétel képzése.

---
+++ virtual_receptionist/Controllers/Exceptions/InvalidBookingException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace virtual_receptionist.Controllers.Exceptions
+{
+    /// <summary>
+    /// Foglalás kivétel
+    /// </summary>
+    public class InvalidBookingException : Exception
+    {
+        #region Konstruktor
+
+        /// <summary>
+        /// Foglalás kivétel konstruktora
+        /// </summary>
+        /// <param name="message">Kivétel szövege</param>
+        public InvalidBookingException(string message) : base(message)
+        {
+        }
+
+        #endregion
+    }
+}
