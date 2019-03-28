41e8887f5e22ef469f1ad417072230e58e47da31
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-03-28 19:20:20 +0100

    Szobakapacitás adott dátumon kivétel és validátor osztály init.

---
+++ virtual_receptionist/Controllers/Exceptions/InvalidCapacityException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace virtual_receptionist.Controllers.Exceptions
+{
+    /// <summary>
+    /// Szobakapacitás kivétel adott napon
+    /// </summary>
+    public class InvalidCapacityException : Exception
+    {
+        #region Konstruktor
+
+        /// <summary>
+        /// Szobakapacitás kivétel adott napon
+        /// </summary>
+        public InvalidCapacityException() : base("A megadott napon nincs szabad kapacitás a kijelölt szobához!")
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ virtual_receptionist/Controllers/Validation/FreeCapacityValidaiton.cs
@@ -0,0 +1,29 @@
+﻿using virtual_receptionist.Controllers.Exceptions;
+
+namespace virtual_receptionist.Controllers.Validation
+{
+    public class FreeCapacityValidaiton
+    {
+        #region Adattagok
+
+        #endregion
+
+        #region Konstruktor
+
+        public FreeCapacityValidaiton()
+        {
+
+        }
+
+        #endregion
+
+        #region Metódusok
+
+        public void dd()
+        {
+            throw new InvalidCapacityException();
+        }
+
+        #endregion
+    }
+}
