f98e4f4b56317235031b42731182f09551b8b3ae
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-15 14:03:05 +0100

    refactor login controller.

--- virtual_receptionist/Controllers/LoginController.cs
+++ virtual_receptionist/Controllers/LoginController.cs
@@ -1,61 +1,14 @@
 ﻿using virtual_receptionist.Repositories;
-using System;
 
 namespace virtual_receptionist.Controllers
 {
-    /// <summary>
-    /// Autentikáció vezérlője
-    /// </summary>
     public class LoginController : Controller
     {
-        #region Adattagok
+        private readonly AccommodationRepository accommodationRepository = new AccommodationRepository();
 
-        /// <summary>
-        /// Szálláshely adattár osztály egy példánya
-        /// </summary>
-        private AccommodationRepository repository;
-
-        #endregion
-
-        #region Konstruktor
-
-        /// <summary>
-        /// Autetikáció vezérlő konstruktora
-        /// </summary>
-        public LoginController()
+        public bool EnterApplication(string accommodationId, string password)
         {
-            repository = new AccommodationRepository();
+            return accommodationRepository.Authentication(accommodationId, password);
         }
-
-        #endregion
-
-        #region Metódusok
-
-        /// <summary>
-        /// Metódus, amely beenged a főmenübe
-        /// </summary>
-        /// <param name="accomodationID">Szálláshely azonosító</param>
-        /// <param name="password">Regisztrációhoz tartozó jelszó</param>
-        /// <param name="connectionType">Adatbáziskapcsolódás típusa</param>
-        /// <exception cref="FailedLoginException"></exception>
-        /// <exception cref="Exception"></exception>
-        public bool EnterApplication(string accomodationID, string password)
-        {
-            try
-            {
-                if (repository.Authentication(accomodationID, password))
-                {
-                    return true;
-                }
-
-                return false;
-            }
-            catch (Exception exception)
-            {
-                throw exception;
-            }
-        }
-
-        #endregion
     }
 }
