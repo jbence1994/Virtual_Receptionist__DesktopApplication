ad74d4ef9b9d7b8a625f3cebb7e6e835d25ea08e
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-15 14:19:14 +0100

    refactor country model and repository.

--- virtual_receptionist/Repositories/CountryRepository.cs
+++ virtual_receptionist/Repositories/CountryRepository.cs
@@ -4,71 +4,31 @@ using virtual_receptionist.Models;
 
 namespace virtual_receptionist.Repositories
 {
-    /// <summary>
-    /// Országok adattár
-    /// </summary>
     public class CountryRepository : Repository
     {
-        #region Adattagok
+        private readonly List<Country> countries = new List<Country>();
 
-        /// <summary>
-        /// Országokat tartalmazó lista
-        /// </summary>
-        private List<Country> countries;
-
-        #endregion
-
-        #region Konstruktor
-
-        /// <summary>
-        /// Országok adattár konstruktora
-        /// </summary>
-        public CountryRepository()
-        {
-            countries = new List<Country>();
-        }
-
-        #endregion
-
-        #region Adatfeltöltő metódusok
-
-        /// <summary>
-        /// Metódus, amely adatbázisból feltölti az országok adatait tartalmazó listát
-        /// </summary>
         private List<Country> UploadCountriesList()
         {
-            string sql = "SELECT * FROM country";
-            DataTable dt = database.DQL(sql);
+            const string sql = "SELECT * FROM country";
+            var dt = database.DQL(sql);
 
             foreach (DataRow row in dt.Rows)
             {
-                string name = row["CountryName"].ToString();
+                var name = row["CountryName"].ToString();
 
-                Country countryInstance = new Country(name);
-                countries.Add(countryInstance);
+                countries.Add(new Country {Name = name});
             }
 
             return countries;
         }
 
-        #endregion
-
-        #region Adatelérési metódusok
-
-        /// <summary>
-        /// Metódus, amely feltölti az országokat tartalmazó listát adatbázisból
-        /// </summary>
-        /// <returns>Az adatokkal feltöltött listával tér vissza a metódus</returns>
         public List<Country> GetCountries()
         {
             if (countries.Count == 0)
-            {
                 UploadCountriesList();
-            }
 
             return countries;
         }
-
-        #endregion
     }
 }
