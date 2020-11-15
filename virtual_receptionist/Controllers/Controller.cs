2ff3eb59168d37eab44555d519853359552c0475
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-15 21:43:10 +0100

    Update Controller.cs

--- virtual_receptionist/Controllers/Controller.cs
+++ virtual_receptionist/Controllers/Controller.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using virtual_receptionist.Repositories;
-using virtual_receptionist.Models;
 
 namespace virtual_receptionist.Controllers
 {
@@ -9,40 +9,14 @@ namespace virtual_receptionist.Controllers
         private readonly CountryRepository countryRepository = new CountryRepository();
         private readonly BookingRepository bookingRepository = new BookingRepository();
 
-        /// <summary>
-        /// Metódus, amely visszaadja az országok neveit adattárból
-        /// </summary>
-        /// <returns>Az országnevekkel feltöltött listával tér vissza a metódus</returns>
         public List<string> GetCountries()
         {
-            List<Country> countries = countryRepository.GetCountries();
-
-            List<string> countryNames = new List<string>();
-
-            foreach (Country country in countries)
-            {
-                countryNames.Add(country.Name);
-            }
-
-            return countryNames;
+            return countryRepository.GetCountries().Select(country => country.Name).ToList();
         }
 
-        /// <summary>
-        /// Metódus, amely visszaadja a szobaszámokat adattárból
-        /// </summary>
-        /// <returns></returns>
         public List<int> GetRooms()
         {
-            List<Room> rooms = bookingRepository.GetRooms();
-
-            List<int> roomNumbers = new List<int>();
-
-            foreach (Room room in rooms)
-            {
-                roomNumbers.Add(room.Number);
-            }
-
-            return roomNumbers;
+            return bookingRepository.GetRooms().Select(room => room.Number).ToList();
         }
     }
 }
